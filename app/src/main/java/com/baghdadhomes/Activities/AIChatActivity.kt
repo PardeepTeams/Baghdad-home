39d5ea9d01ddaf7087b13a8229f4567e52a68032
Gaurav Attri <gauravattri620@gmail.com>
2025-09-06 14:01:55 +0530

    Fixed scroll sides issue for project cities list, feature ads and reels

--- app/src/main/java/com/baghdadhomes/Activities/AIChatActivity.kt
+++ app/src/main/java/com/baghdadhomes/Activities/AIChatActivity.kt
@@ -1,16 +1,55 @@
 package com.baghdadhomes.Activities
 
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.baghdadhomes.Models.ModelAiSearchResponse
 import com.baghdadhomes.R
+import com.baghdadhomes.Utils.Utility
 
 class AIChatActivity : AppCompatActivity() {
+    lateinit var imgBack : ImageView
+    lateinit var imgSendMessage : ImageView
+    lateinit var etMessage : EditText
+    lateinit var rvAiChat : RecyclerView
+
+    private val chatList : ArrayList<ModelAiSearchResponse> = ArrayList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_aichat)
+        inits()
+        clickListeners()
+    }
 
+    private fun inits() {
+        imgBack = findViewById(R.id.imgBack)
+        imgSendMessage = findViewById(R.id.imgSendMessage)
+        etMessage = findViewById(R.id.etMessage)
+        rvAiChat = findViewById(R.id.rvAiChat)
+
+        chatList.add(ModelAiSearchResponse(message = getString(R.string.ai_chat_default_message_1)))
+        chatList.add(ModelAiSearchResponse(message = getString(R.string.ai_chat_default_message_2)))
+
+        rvAiChat.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL,false)
+    }
+
+    private fun clickListeners(){
+        imgBack.setOnClickListener {
+            finish()
+        }
+
+        imgSendMessage.setOnClickListener {
+            if (etMessage.text.trim().isEmpty()) {
+                Utility.showToast(this, getString(R.string.enter_message))
+                return@setOnClickListener
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/baghdadhomes/Models/ModelAiSearchResponse.kt
@@ -0,0 +1,11 @@
+package com.baghdadhomes.Models
+
+import com.google.gson.annotations.SerializedName
+
+class ModelAiSearchResponse (
+    @SerializedName("success") var success : Boolean? = null,
+    @SerializedName("message") var message : String? = null,
+    @SerializedName("search_text") var searchText : String? = null,
+    @SerializedName("count") var count : Int? = null,
+    @SerializedName("result") var result : List<Result>? = null,
+)
